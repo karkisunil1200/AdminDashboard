c2b5141a03ed93fe24bedaae502420120c50f9a1
sunilkarki <karkisunil1200@gmail.com>
2022-02-12 18:15:01 -0600

    First commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./topbar.css";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">KarkiAdmin</span>
+        </div>
+
+        <div className="topRight"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
