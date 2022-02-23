fc2cc8074f203cae673ee28e06e0aae4fe21990f
sunilkarki <karkisunil1200@gmail.com>
2022-02-23 08:35:45 -0600

    Added basic table in userlist component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <div className="container">
         <Sidebar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/users" element={<UserList />} />
         </Routes>
       </div>
     </Router>
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,8 +1,78 @@
 import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
 import "./userList.css";
 
 const UserList = () => {
-  return <div>UserList</div>;
+  const columns = [
+    { field: "id", headerName: "ID", width: 130 },
+    { field: "lastName", headerName: "Username", width: 200 },
+    { field: "email", headerName: "Email", width: 200 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 120,
+    },
+    {
+      field: "transaction",
+      headerName: "Transaction Volume",
+      width: 160,
+    },
+  ];
+  const rows = [
+    {
+      id: 1,
+      lastName: "John Snow",
+      avatar: "",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 2,
+      lastName: "John Snow",
+      avatar: "",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 3,
+      lastName: "John Snow",
+      avatar: "",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 4,
+      lastName: "John Snow",
+      avatar: "",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 5,
+      lastName: "John Snow",
+      avatar: "",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+  ];
+
+  return (
+    <div className="userList">
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
 };
 
 export default UserList;
