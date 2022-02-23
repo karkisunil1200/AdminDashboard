a4683093f77ffac7ef92335bd8c5ced24f767b69
sunilkarki <karkisunil1200@gmail.com>
2022-02-23 08:51:37 -0600

    Added styles for the table

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -2,11 +2,24 @@ import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 import "./userList.css";
+import { DeleteOutline } from "@mui/icons-material";
 
 const UserList = () => {
   const columns = [
     { field: "id", headerName: "ID", width: 130 },
-    { field: "lastName", headerName: "Username", width: 200 },
+    {
+      field: "user",
+      headerName: "Username",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="userListUser">
+            <img src={params.row.avatar} alt="" className="userListImg" />
+            {params.row.userName}
+          </div>
+        );
+      },
+    },
     { field: "email", headerName: "Email", width: 200 },
     {
       field: "status",
@@ -18,44 +31,97 @@ const UserList = () => {
       headerName: "Transaction Volume",
       width: 160,
     },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 160,
+      renderCell: (params) => {
+        return (
+          <>
+            <button className="userListEdit">Edit</button>
+            <DeleteOutline className="userListDelete" />
+          </>
+        );
+      },
+    },
   ];
   const rows = [
     {
       id: 1,
-      lastName: "John Snow",
-      avatar: "",
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
       email: "john@gmail.com",
       status: "active",
       transaction: "$120.34",
     },
     {
       id: 2,
-      lastName: "John Snow",
-      avatar: "",
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
       email: "john@gmail.com",
       status: "active",
       transaction: "$120.34",
     },
     {
       id: 3,
-      lastName: "John Snow",
-      avatar: "",
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
       email: "john@gmail.com",
       status: "active",
       transaction: "$120.34",
     },
     {
       id: 4,
-      lastName: "John Snow",
-      avatar: "",
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
       email: "john@gmail.com",
       status: "active",
       transaction: "$120.34",
     },
     {
       id: 5,
-      lastName: "John Snow",
-      avatar: "",
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 6,
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 7,
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 8,
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 9,
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
+      email: "john@gmail.com",
+      status: "active",
+      transaction: "$120.34",
+    },
+    {
+      id: 10,
+      userName: "John Snow",
+      avatar: "https://images7.alphacoders.com/650/650008.jpg",
       email: "john@gmail.com",
       status: "active",
       transaction: "$120.34",
@@ -66,8 +132,9 @@ const UserList = () => {
     <div className="userList">
       <DataGrid
         rows={rows}
+        disableSelectionOnClick
         columns={columns}
-        pageSize={5}
+        pageSize={8}
         rowsPerPageOptions={[5]}
         checkboxSelection
       />
